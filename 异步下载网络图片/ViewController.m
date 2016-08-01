eed21ccc39190b84a0cb757c29d2b14f980c17db
whong7 <727782268@qq.com>
2016-08-01 09:39:10 +0800

    Initial Commit

---
+++ 异步下载网络图片/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  异步下载网络图片
+//
+//  Created by whong7 on 16/8/1.
+//  Copyright © 2016年 whong7. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 异步下载网络图片/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  异步下载网络图片
+//
+//  Created by whong7 on 16/8/1.
+//  Copyright © 2016年 whong7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
